8a2c685c4fb19c53d4b4617d716350f90e8a034f
Ziggy Cross <ziggycross@me.com>
2023-05-16 01:57:32 -0700

    Created basic render pipeline.

--- app.cpp
+++ app.cpp
@@ -6,6 +6,21 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\n";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(0.5f, 0.5f, 0.5, 1.0f);\n"
+    "}\n";
+
+
 int main()
 {
     // Set GLFW Settings
@@ -17,13 +32,14 @@ int main()
 
     // Create OpenGL Window
     GLFWwindow* window = glfwCreateWindow(800, 600, "Campfire", NULL, NULL);
-    if (window == NULL)
+    if(window == NULL)
     {
         std::cout << "Failed to create a window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Init GLAD
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -32,8 +48,85 @@ int main()
         return -1;
     }
 
-    // Add callbacks
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // SHADERS
+
+    // Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // -- CHECK SHADER COMPILED PROPERLY
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // -- CHECK SHADER COMPILED PROPERLY
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // -- CHECK PROGRAM COMPILED PROPERLY
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // -- Delete shaders after compiling
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    // -- Set shader program to be used
+
+    // END SHADERS
+
+    
+    // Geometry
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // 'Vertex Array Object' - stores VBOs
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    
+    // 'Vertex Buffer Object' - stores vertices on GPU memory
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // Bind vertext array
+    glBindVertexArray(VAO);
+    
+    // Bind buffer and add geometry
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Configure vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind buffer and array
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // Render loop
     while(!glfwWindowShouldClose(window))
@@ -44,6 +137,11 @@ int main()
         // Render commands
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Draw
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         // Swapp buffers and call events
         glfwSwapBuffers(window);
